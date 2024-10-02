48bdd24a9ec96558ec8bc335cc8a35dd7f20aa55
HildaAb <ilona.aase@gmail.com>
2024-10-02 12:24:05 +0200

    changed size and position in css

--- app.js
+++ app.js
@@ -76,9 +76,10 @@ function displayForecast(response) {
         forecastHtml +
         `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+      <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        <span class="weather-forecast-date">${formatDay(day.time)}</span>
 
-        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
             <strong>${Math.round(day.temperature.maximum)}º</strong>
